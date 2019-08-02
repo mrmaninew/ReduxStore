2f0b69268caf1bdd825bb8ac8426e3b0a1cb102b
mrmaninew <manikanta@live.in>
2019-08-02 20:59:57 +0530

    add reducers and seperated behaviours

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./store/reducer";
+import counterReducer from "./store/reducers/counter";
+import resultsReducer from "./store/reducers/result";
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultsReducer
+});
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
